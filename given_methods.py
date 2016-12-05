9e245e31b3ac730e9575ec6b439a4c3fba6216f5
theWebby <jrwebb1@sheffield.ac.uk>
2016-12-05 19:43:12 +0000

    Added given_methods.py to include methods given to complete the assignment. These methods have been taken from SageMathCloud.

---
+++ given_methods.py
@@ -0,0 +1,23 @@
+# Methods that have been supplied to help complete the assignment
+
+import numpy as np
+
+
+def classify(train, train_labels, test):
+    """Nearest neighbour classification
+
+    train - matrix of training data (one sample per row)
+    train_labels - corresponding training data labels
+    test - matrix of samples to classify
+
+    returns: labels - vector of test data labels
+    """
+
+    x = np.dot(test, train.transpose())
+    modtest = np.sqrt(np.sum(test*test, axis=1))
+    modtrain = np.sqrt(np.sum(train*train, axis=1))
+    dist = x / np.outer(modtest, modtrain.transpose())  # cosine distance
+    nearest = np.argmax(dist, axis=1)
+    labels = train_labels[nearest]
+
+    return labels
