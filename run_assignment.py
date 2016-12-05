d3b76c48e45973b35fc5290588dec51c3ab29382
theWebby <jrwebb1@sheffield.ac.uk>
2016-12-05 18:29:51 +0000

    added some needed imports and extracted all of the data from the pkl file.

---
+++ run_assignment.py
@@ -0,0 +1,9 @@
+import pickle
+import numpy as np
+
+data = pickle.load(open("data/assignment2.pkl", "rb"))
+train_data = data['train_data']
+train_labels = data['train_labels']
+test1 = data['test1']
+test2 = data['test2']
+words = data['words']
